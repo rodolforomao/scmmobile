03f686eea74f883fc871ad3efc53c011e570ac2f
rodolforomao <rodolforomao@gmail.com>
2020-12-17 10:42:48 -0300

    alteracoes no mac

--- scm_engenharia_app/ios/Runner/AppDelegate.swift
+++ scm_engenharia_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
 import UIKit
+import WebKit
 import Flutter
+import GoogleMaps
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,9 +10,23 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
     if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+      // For iOS 10 display notification (sent via APNS)
+      UNUserNotificationCenter.current().delegate = self
+
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: {_, _ in })
+    } else {
+      let settings: UIUserNotificationSettings =
+      UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
     }
+    application.registerForRemoteNotifications()
+    GMSServices.provideAPIKey("AIzaSyD5F1TUO3uHBb_EB7xqhbPCRBNynO-8gHg")
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
