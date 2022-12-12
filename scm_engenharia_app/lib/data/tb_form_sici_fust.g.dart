e76dbb19e689995da74ac91544889900321d47ef
allessandrojs <allessandrosj@gmail.com>
2022-12-12 19:36:57 -0300

    Alteracao sqlite
    Alteracao sqlite

--- scm_engenharia_app/lib/data/tb_form_sici_fust.g.dart
+++ scm_engenharia_app/lib/data/tb_form_sici_fust.g.dart
@@ -6,44 +6,44 @@ part of 'tb_form_sici_fust.dart';
 // RealmObjectGenerator
 // **************************************************************************
 
-class TbFormSiciFust extends _TbFormSiciFust  with RealmEntity, RealmObjectBase, RealmObject {
+class TbFormSiciFust extends _TbFormSiciFust  with RealmEntity, RealmObject {
   TbFormSiciFust(
       ObjectId idFormSiciFustApp,
       String? idRegistro,
       String? result,
       ) {
-    RealmObjectBase.set(this, 'idFormSiciFustApp', idFormSiciFustApp);
-    RealmObjectBase.set(this, 'idRegistro', idRegistro);
-    RealmObjectBase.set(this, 'result', result);
+    RealmObject.set(this, 'idFormSiciFustApp', idFormSiciFustApp);
+    RealmObject.set(this, 'idRegistro', idRegistro);
+    RealmObject.set(this, 'result', result);
   }
 
   TbFormSiciFust._();
 
   @override
-  ObjectId get idFichaSiciApp => RealmObjectBase.get<ObjectId>(this, 'idFormSiciFustApp') as ObjectId;
+  ObjectId get idFichaSiciApp => RealmObject.get<ObjectId>(this, 'idFormSiciFustApp') as ObjectId;
   @override
   set idFichaSiciApp(ObjectId value) => throw RealmUnsupportedSetError();
 
   @override
-  String get idRegistro => RealmObjectBase.get<String>(this, 'idRegistro') as String;
+  String get idRegistro => RealmObject.get<String>(this, 'idRegistro') as String;
   @override
-  set idRegistro(String value) => RealmObjectBase.set(this, 'idRegistro', value);
+  set idRegistro(String value) => RealmObject.set(this, 'idRegistro', value);
 
   @override
-  String get result => RealmObjectBase.get<String>(this, 'result') as String;
+  String get result => RealmObject.get<String>(this, 'result') as String;
   @override
-  set result(String value) => RealmObjectBase.set(this, 'result', value);
+  set result(String value) => RealmObject.set(this, 'result', value);
 
 
 
   @override
-  Stream<RealmObjectChanges<TbFormSiciFust>> get changes => RealmObjectBase.getChanges<TbFormSiciFust>(this);
+  Stream<RealmObjectChanges<TbFormSiciFust>> get changes => RealmObject.getChanges<TbFormSiciFust>(this);
 
   static SchemaObject get schema => _schema ??= _initSchema();
   static SchemaObject? _schema;
   static SchemaObject _initSchema() {
-    RealmObjectBase.registerFactory(TbFormSiciFust._);
-    return const SchemaObject(ObjectType.realmObject,TbFormSiciFust, 'TbFormSiciFust', [
+    RealmObject.registerFactory(TbFormSiciFust._);
+    return const SchemaObject(TbFormSiciFust, '_TbFormSiciFust', [
       SchemaProperty('idFormSiciFustApp', RealmPropertyType.objectid, mapTo: 'idFormSiciFustApp', primaryKey: true),
       SchemaProperty('idRegistro', RealmPropertyType.string),
       SchemaProperty('result', RealmPropertyType.string),
