aa6a42c9a77df8b4406f50b7ec947c0b76b27483
allessandrojs <allessandrosj@gmail.com>
2024-02-01 20:13:14 -0300

    Update selecione_arquivo_dici_fust_view.dart
    Correção da planilha  upload

--- scm_engenharia_app/lib/views/sici_views/selecione_arquivo_dici_fust_view.dart
+++ scm_engenharia_app/lib/views/sici_views/selecione_arquivo_dici_fust_view.dart
@@ -52,11 +52,8 @@ class SelecioneArquivoDiciFustState extends State<SelecioneArquivoDiciFustView> 
             } else {
               Map<String, dynamic>  mapResult = resultRest.result as Map<String, dynamic>;
               List mapdadosEmServicos = [];
-              if(Components.onIsEmpty(mapResult['data']['Dici']) != '')
+              if(Components.onIsEmpty(mapResult['data']?['Dici']) != '')
                 {
-
-
-                 print(mapResult['data']);
                   for (var item in mapResult['data']['Dici'] as List) {
                     mapdadosEmServicos.add({
                       'idLancamento': '',
@@ -76,60 +73,31 @@ class SelecioneArquivoDiciFustState extends State<SelecioneArquivoDiciFustView> 
 
               if(Components.onIsEmpty(mapResult['data']) != '')
                 {
-
-                  if(Components.onIsEmpty(mapResult['data']['Empresa']) != '')
-                  {
-
-                  }
-
-                  var dsf = InputSiciFileModel.fromJson({
-
-
-
-                    'razaoSocial': Components.onIsEmpty(mapResult['data']['Empresa'] ['razao_social']),
-                    'telefoneFixo': Components.onIsEmpty(mapResult['data']['Empresa']['telefone_fixo']),
-                    'telefoneMovel': Components.onIsEmpty(mapResult['data']['Empresa']['telefone_celular']),
-                    'cnpj': Components.onIsEmpty(mapResult['data']['Empresa']['cnpj']),
-                    'receitaBruta': Components.onIsEmpty(mapResult['data']['Financeiro']['bruta']),
-
-                    'simplesPorc ': Components.onIsEmpty(mapResult['data1']['Financeiro']['simples']) == '' ? '' : Components.onIsEmpty(mapResult['data']['Financeiro']['simples'][0]),
-
-                    'simplesPorc ': Components.onIsEmpty(mapResult['data']['Financeiro']['simples'][0]),
-                    'simples': Components.onIsEmpty(mapResult['data']['Financeiro']['simples'][1]),
-                    'icmsPorc ': Components.onIsEmpty(mapResult['data']['Financeiro']['icms'][0]),
-                    'icms': Components.onIsEmpty(mapResult['data']['Financeiro']['icms'][1]),
-                    'pis': Components.onIsEmpty(mapResult['data']['Financeiro']['pis']),
-                    'cofins': Components.onIsEmpty(mapResult['data']['Financeiro']['cofins']),
-                    'receitaLiquida': Components.onIsEmpty(mapResult['data']['Financeiro']['líquida']),
-                    'dadosEmServicos':mapdadosEmServicos,
+                  Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (context) =>
+                            FormularioDiciFustView(map:{
+                               'formulario':InputSiciFileModel.fromJson({
+                                'razaoSocial': Components.onIsEmpty(mapResult['data']?['Empresa']?['razao_social']),
+                                'telefoneFixo': Components.onIsEmpty(mapResult['data']?['Empresa']?['telefone_fixo']),
+                                'telefoneMovel': Components.onIsEmpty(mapResult['data']?['Empresa']?['telefone_celular']),
+                                'cnpj': Components.onIsEmpty(mapResult['data']?['Empresa']?['cnpj']),
+                                'receitaBruta': Components.onIsEmpty(mapResult['data']?['Financeiro']?['bruta']),
+                                'simplesPorc ': Components.onIsEmpty(mapResult['data']?['Financeiro']?['simples']?[0]),
+                                'simples': Components.onIsEmpty(mapResult['data']?['Financeiro']?['simples']?[1]),
+                                'icmsPorc ': Components.onIsEmpty(mapResult['data']?['Financeiro']?['icms']?[0]),
+                                'icms': Components.onIsEmpty(mapResult['data']?['Financeiro']?['icms']?[1]),
+                                'pis': Components.onIsEmpty(mapResult['data']?['Financeiro']?['pis']),
+                                'cofins': Components.onIsEmpty(mapResult['data']?['Financeiro']?['cofins']),
+                                'receitaLiquida': Components.onIsEmpty(mapResult['data']?['Financeiro']?['líquida']),
+                                'dadosEmServicos':mapdadosEmServicos,
+                              }),
+                              'isLancamentosComBaseMesAnterior':false,
+                            }),
+                      )).then((value) {
                   });
                 }
-
-
-              Navigator.push(
-                  context,
-                  CupertinoPageRoute(
-                    builder: (context) =>
-                        FormularioDiciFustView(map:{
-                            'formulario':InputSiciFileModel.fromJson({
-                            'razaoSocial': Components.onIsEmpty(mapResult['data']['Empresa']['razao_social']),
-                            'telefoneFixo': Components.onIsEmpty(mapResult['data']['Empresa']['telefone_fixo']),
-                            'telefoneMovel': Components.onIsEmpty(mapResult['data']['Empresa']['telefone_celular']),
-                            'cnpj': Components.onIsEmpty(mapResult['data']['Empresa']['cnpj']),
-                            'receitaBruta': Components.onIsEmpty(mapResult['data']['Financeiro']['bruta']),
-                              'simplesPorc ': Components.onIsEmpty(mapResult['data']['Financeiro']['simples'][0]),
-                              'simples': Components.onIsEmpty(mapResult['data']['Financeiro']['simples'][1]),
-                            'icmsPorc ': Components.onIsEmpty(mapResult['data']['Financeiro']['icms'][0]),
-                              'icms': Components.onIsEmpty(mapResult['data']['Financeiro']['icms'][1]),
-                            'pis': Components.onIsEmpty(mapResult['data']['Financeiro']['pis']),
-                            'cofins': Components.onIsEmpty(mapResult['data']['Financeiro']['cofins']),
-                            'receitaLiquida': Components.onIsEmpty(mapResult['data']['Financeiro']['líquida']),
-                            'dadosEmServicos':mapdadosEmServicos,
-                          }),
-                            'isLancamentosComBaseMesAnterior':false,
-                        }),
-                  )).then((value) {
-              });
             }
           }
       }
